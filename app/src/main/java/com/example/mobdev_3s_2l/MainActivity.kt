54c9dcc2846fbcd270125fc22d0f09b558626ce3
Not-Saint-Paul <145997767+Not-Saint-Paul@users.noreply.github.com>
2025-06-28 15:53:08 +0700

    Добавлены EditText и кнопки, меняющие атрибуты EditText'а

--- app/src/main/java/com/example/mobdev_3s_2l/MainActivity.kt
+++ app/src/main/java/com/example/mobdev_3s_2l/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mobdev_3s_2l
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,38 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val editText = findViewById<EditText>(R.id.edit_text_id)
+        val buttonBlackText = findViewById<Button>(R.id.button_black_text)
+        val buttonRedText = findViewById<Button>(R.id.button_red_text)
+        val buttonSizeTo8sp = findViewById<Button>(R.id.button_to8sp)
+        val buttonSizeTo24sp = findViewById<Button>(R.id.button_to24sp)
+        val buttonWhiteBackground = findViewById<Button>(R.id.button_white_background)
+        val buttonYellowBackground = findViewById<Button>(R.id.button_yellow_background)
+
+        buttonBlackText.setOnClickListener {
+            editText.setTextColor(Color.BLACK)
+        }
+
+        buttonRedText.setOnClickListener {
+            editText.setTextColor(Color.RED)
+        }
+
+        buttonSizeTo8sp.setOnClickListener {
+            editText.setTextSize(8f)
+        }
+
+        buttonSizeTo24sp.setOnClickListener {
+            editText.setTextSize(24f)
+        }
+
+        buttonWhiteBackground.setOnClickListener {
+            editText.setBackgroundColor(Color.WHITE)
+        }
+
+        buttonYellowBackground.setOnClickListener {
+            editText.setBackgroundColor(Color.YELLOW)
         }
     }
 }
